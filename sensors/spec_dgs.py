8bcfca517aacba816b2180745c5f58a027c29292
Clint <55072157+clintonbf@users.noreply.github.com>
2021-05-12 14:54:02 -0700

    Added base code for a SPEC DGS sensor

---
+++ sensors/spec_dgs.py
@@ -0,0 +1,28 @@
+import SensorInterface
+import serial
+
+
+class spec_dgs(SensorInterface):
+    """
+        Credit for much of this code goes to Noah MacRitchie (noah21mac@gmail.com)
+        and Andrey Goryelov (andrey.goryelov@gmail.com)
+    """
+
+    def __init__(self, device: str, timeout: int, baud_rate: int):
+        self.__start_reading = 'c'
+        self.__end_reading = 'R'
+        self.__device = device
+        self.__timeout = timeout
+        self.__baud_rate = baud_rate
+
+    def connect_to_port(self) -> serial:
+        ser = serial.Serial(self.__device, self.__baud_rate, timeout=self.__timeout, parity=serial.PARITY_NONE)
+        return ser
+
+    def get_raw_data(self) -> bytes:
+        ser = self.connect_to_port()
+        ser.write(self.__start_reading.encode())
+        line = ser.readline()
+        ser.write(self.__end_reading.encode())
+
+        return line
